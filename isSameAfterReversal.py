c3b352395f5a37c1b8384bc8dcb60ef0e600895a
Swetha-20-2005 <swethasubramaniyam20@gmail.com>
2026-02-21 20:25:25 +0530

    Create isSameAfterReversal.py

---
+++ isSameAfterReversal.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+        a=num
+        rev=0
+        while(num!=0):
+            d=num%10
+            rev=rev*10+d
+            num=num//10
+        if(len(str(a))==len(str(rev))):
+            return True
+        else:
+            return False
+print(Solution().isSameAfterReversals(526)) 
+
+output:
+Example 1:
+
+Input: num = 526
+Output: true
+Explanation: Reverse num to get 625, then reverse 625 to get 526, which equals num.
+Example 2:
+
+Input: num = 1800
+Output: false
+Explanation: Reverse num to get 81, then reverse 81 to get 18, which does not equal num.
+Example 3:
+
+Input: num = 0
+Output: true
+Explanation: Reverse num to get 0, then reverse 0 to get 0, which equals num.
