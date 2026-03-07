918c81a2023224ae25055a324c949d580273105d
Swetha-20-2005 <swethasubramaniyam20@gmail.com>
2026-03-07 19:21:46 +0530

    Create firstbadversion.py

---
+++ firstbadversion.py
@@ -0,0 +1,32 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+        
+        while low < high:
+            mid = (low + high) // 2
+            
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid + 1
+        
+        return low
+
+OUTPUT:
+Example 1:
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+Example 2:
+
+Input: n = 1, bad = 1
+Output: 1
