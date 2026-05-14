b84680b0b2ce1640d21ac4c3f295382c4b205226
Swetha-20-2005 <swethasubramaniyam20@gmail.com>
2026-05-14 21:51:40 +0530

    Create countDigits.py

---
+++ countDigits.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        b=str(num)
+        count=0
+        for i in b:
+            if(num%int(i)==0):
+                count+=1
+        return count    
+
+OUTPUT:
+Example 1:
+
+Input: num = 7
+Output: 1
+Explanation: 7 divides itself, hence the answer is 1.
+Example 2:
+
+Input: num = 121
+Output: 2
+Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+Example 3:
+
+Input: num = 1248
+Output: 4
+Explanation: 1248 is divisible by all of its digits, hence the answer is 4.
