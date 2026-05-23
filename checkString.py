891f901d18d6ad6a78ccb5415c72643bdea3ed38
Swetha-20-2005 <swethasubramaniyam20@gmail.com>
2026-05-23 19:29:49 +0530

    Create checkString.py

---
+++ checkString.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        a="".join(sorted(s))
+        return s==a
+OUTPUT:
+Input: s = "aaabbb"
+Output: true
+Explanation:
+The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
+Hence, every 'a' appears before every 'b' and we return true.
+Example 2:
+
+Input: s = "abab"
+Output: false
+Explanation:
+There is an 'a' at index 2 and a 'b' at index 1.
+Hence, not every 'a' appears before every 'b' and we return false.
+Example 3:
+
+Input: s = "bbb"
+Output: true
+Explanation:
+There are no 'a's, hence, every 'a' appears before every 'b' and we return true.
+
